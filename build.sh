aa641500aa8215685581d112b479e7fc7c8196e1
blabla-yy <yahaha7777@gmail.com>
2023-07-14 18:03:37 +0800

    remove unused

--- build.sh
+++ build.sh
@@ -7,7 +7,3 @@ lipo -create \
 ./target/x86_64-apple-darwin/release/libNetworkTraffic.a \
 ./target/aarch64-apple-darwin/release/libNetworkTraffic.a \
 -output libNetworkTraffic.a
-
-echo "codesign"
-# 循环签名
-codesign -f -s "Apple Development: 563335734@qq.com (Q5E96A7VL4)" libNetworkTraffic.a
