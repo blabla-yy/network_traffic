195ec2e1aa05a7393e70c7f0997b3accb3cac278
blabla-yy <yahaha7777@gmail.com>
2023-11-13 17:35:07 +0800

    lipo

--- build.sh
+++ build.sh
@@ -7,3 +7,8 @@ cargo build --target x86_64-apple-darwin --release
 #./target/x86_64-apple-darwin/release/libNetworkTraffic.a \
 #./target/aarch64-apple-darwin/release/libNetworkTraffic.a \
 #-output libNetworkTraffic.a
+
+lipo -create \
+./target/x86_64-apple-darwin/release/network-traffic \
+./target/aarch64-apple-darwin/release/network-traffic \
+-output network-traffic
